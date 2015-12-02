4ef863444651565dbf48b452b7976618370f1037
Joel Cardinal <joelcardinal@hotmail.com>
2015-12-02 12:27:24 -0700

    Added additional utility method, refresh()

--- js/superCuteForms.js
+++ js/superCuteForms.js
@@ -1,7 +1,7 @@
 /*
 *
 *	superCuteForms.js
-*
+*	https://github.com/joelcardinal/superCuteForms
 *	jQuery +1.7 plugin
 *
 *	(\__/)     [x]
@@ -35,7 +35,7 @@
                 if (type != 'select') {
                     $formElem.addClass('superCuteFormElem').wrap('<div class="' + type + ' superCuteFormElemWrapper"></div>');
                     $formElem.parent().append('<div class="' + checkedState($formElem) + ' superCuteFormElemWrapper"></div>');
-                    $formElem.on("click", function () {
+                    $formElem.on("change", function () {
                         if (type === "checkbox") {
                             $formElem.next('div').removeClass().addClass((checkedState($formElem) + ' superCuteFormElemWrapper'));
                         } else {
@@ -72,22 +72,85 @@
         return $('.superCuteFormElem');
     }
 
+    $.superCuteForms.refresh = function () {
+        var arr = [];
+
+        $('body').find('input[type="checkbox"], input[type="radio"], select').each(function () {
+
+            var $formElem = $(this);
+
+            if ($formElem.hasClass('superCuteFormElem')) {
+                var type = $formElem.attr('type') || 'select',
+					classToRemove,
+                    checkedState = function ($inputElem) {
+                        var state;
+                        if ($inputElem[0].checked) {
+							state = "checked";
+							classToRemove = "unchecked";
+						} else {
+							state = "unchecked";
+							classToRemove = "checked";
+						}
+                        return state;
+                    };
+                arr.push($formElem);
+                if (type != 'select') {
+					var state = checkedState($formElem);
+                    if (type === "checkbox" && !$formElem.next('div').hasClass(state)) {
+                        $formElem.next('div').removeClass(classToRemove).addClass(state);
+                    } else {
+                        $.each($('input[name="' + $formElem.attr('name') + '"]'), function () {
+                            var $radio = $(this),
+							state = checkedState($radio);
+							if (!$formElem.next('div').hasClass(state)) {
+                            	$radio.next('div').removeClass(classToRemove).addClass(state);
+							}
+                        });
+                    }
+                }
+            }
+
+        });
+
+        // returns affected form elements as a jQuery collection, NOT a collection of the new parent elements
+        return $(arr);
+    }
+
 }(jQuery));
 
 /*
 
 !!! USAGE EXAMPLES !!!
 
-Usage example:
-$( "body" ).superCuteForms();
+Example:
 
-Usage example of undoing any changes made by superCuteForms
-$.superCuteForms.undo();
+	$( "body" ).superCuteForms();
 
-Usage example of undoing any changes made by superCuteForms within optional selection query (won't affect elements outside selection):
-$.superCuteForms.undo('div.myClass');
+Example of undoing any changes made by superCuteForms:
 
-Usage example of retrieving jQuery collection of all elements on page affected by superCuteForms
-$.superCuteForms.affected();
+	$.superCuteForms.undo();
+
+Example of undoing any changes made by superCuteForms within optional
+selection query (won't affect elements outside selection):
+
+	$.superCuteForms.undo('div.myClass');
+
+Example of retrieving jQuery collection of all elements on page
+affected by superCuteForms, including additional divs added by plugin:
+
+	$.superCuteForms.affected();
+
+Example of correcting checkbox and radio selected/checked status, after
+javascript is used to alter the status of a form element if jQuery's
+change() method isn't used:
+
+	$.superCuteForms.refresh();
+
+Using refresh() won't be necessary if you alter checkbox and radio
+selected/checked status using change(), for example:
+
+$('input[type="checkbox"]').attr('checked','true').change();
+
+var $radios = $('input:radio[name=r1]'); $radios.filter('[value=myRadio1]').prop('checked', true).change();
 
 */
